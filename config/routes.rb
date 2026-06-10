44121a63d159430e84ff125fffbb0ad63c6b3cb0
Xavier Ducourneau <xavierdcrn@gmail.com>
2026-06-10 14:53:52 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :plants
+  resources :plants do
     resources :chats, only: [:create, :show] do
       resources :messages, only: [:create]
     end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
