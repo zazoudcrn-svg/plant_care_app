fec42b23cae6ba258a64d6ff0d295debf105fa66
rupa yadav <rupayadav0890@gmail.com>
2026-06-12 14:35:42 +0200

    Updated related to images upload

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,7 +1,7 @@
 class Plant < ApplicationRecord
   belongs_to :user
   has_many :chats, dependent: :destroy
-  has_many_attached :photos
+  has_one_attached :photo
 
   def days_since_last_watered
     return nil if last_watered_on.blank?
